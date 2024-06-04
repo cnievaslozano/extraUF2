7d03e53276c3f47aa3ae545eda02aaff598f8d6f
cnievaslozano <cnievaslozano@gmail.com>
2024-06-04 20:19:04 +0200

    reinici animacio

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('play').addEventListener('click', function () {
+    document.getElementById('playButton').addEventListener('click', function () {
         let nave = document.getElementById('nave');
         let audio = document.getElementById('audio');
 
-        nave.style.animation = 'landing 3s ease-in-out infinite';
+        // Para reiniciar la animación
+        nave.style.animation = 'none';
+        // Forzar reflujo
+        nave.offsetHeight; 
+        // Restablecer la animación
+        nave.style.animation = 'landing 3s ease-in-out forwards';
+
+        audio.currentTime = 0; // Reinicia el audio
         audio.play();
     });
 });
